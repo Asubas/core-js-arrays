ae979582de3e314814c2b06358cf9798a315b432
unknown <znaxor74@mail.ru>
2024-01-07 17:58:39 +0300

    fix: done optimak implementation tasks

--- src/test.js
+++ src/test.js
@@ -1,5 +1,11 @@
-// function getHead(arr, n) {
-//   return arr.slice(-n);
+// function createNDimensionalArray(n, size) {
+//   if (n === 1) {
+//     return Array.from({ length: size }).fill(0);
+//   }
+//   return Array.from({ length: size }, () =>
+//     createNDimensionalArray(n - 1, size)
+//   );
 // }
 
-// console.log(getHead(['c', 'a', 't'], 0));
+// console.log(createNDimensionalArray(2, 3));
+// // [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
