d8fa40c116f508ccda4bdb4619375b88a4d3fc96
unknown <znaxor74@mail.ru>
2024-01-08 22:44:57 +0300

    done propagateItemsBy..

--- src/test.js
+++ src/test.js
@@ -1,17 +1,7 @@
-// function findLongestIncreasingSubsequence(nums) {
-//   let maxLength = 1;
-//   let currentLength = 1;
-//   return nums.reduce((acc, current, index) => {
-//     if (current < nums[index + 1]) {
-//       currentLength += 1;
-//       maxLength = Math.max(maxLength, currentLength);
-//     } else {
-//       currentLength = 1;
-//     }
-//     return maxLength;
-//   }, 0);
+// function propagateItemsByPositionIndex(arr) {
+//   return arr.flatMap((element, index) => {
+//     return new Array(index + 1).fill(element);
+//   });
 // }
 
-// console.log(
-//   findLongestIncreasingSubsequence([41, 60, 80, 10, 22, 9, 33, 21, 50])
-// );
+// console.log(propagateItemsByPositionIndex([1, 2, 3, 4, 5]));
