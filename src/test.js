3f6cc4218687856269bd98a13dd5911cac4e3573
unknown <znaxor74@mail.ru>
2024-01-08 23:14:17 +0300

    done task shiftArray

--- src/test.js
+++ src/test.js
@@ -1,7 +1,15 @@
-// function propagateItemsByPositionIndex(arr) {
-//   return arr.flatMap((element, index) => {
-//     return new Array(index + 1).fill(element);
-//   });
+// function shiftArray(arr, n) {
+//   if (n > 0) {
+//     const newArr = arr.slice(0, n + 1);
+//     const newArr2 = arr.splice(arr.length - n);
+//     return newArr2.concat(newArr);
+//   }
+//   const newArr = arr.slice(-n);
+//   const newArr2 = arr.slice(0, -n);
+//   return newArr.concat(newArr2);
 // }
 
-// console.log(propagateItemsByPositionIndex([1, 2, 3, 4, 5]));
+// // console.log(shiftArray(['a', 'b', 'c', 'd'], -1));
+// console.log(shiftArray([10, 20, 30, 40, 50], -3));
+// // shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
+// // ['b', 'c', 'd', 'a'],
